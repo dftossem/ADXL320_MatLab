eb9fb3e53b837db00a2dc5c3a3b2b594dca622fd
dftossem <dftossem@unal.edu.co>
2017-11-29 21:15:48 -0500

    Update plotAcc2D.m
    Plot and save real-time data from arduino with pitch and roll values in g [m/s^2]

--- plotAcc2D.m
+++ plotAcc2D.m
@@ -11,52 +11,34 @@ set(s, 'Parity', 'none');
 set(s, 'DataBits', 8);
 set(s, 'StopBit', 1);
 set(s, 'Timeout',10);
-set(s, 'Terminator',';');
+% set(s, 'Terminator',';');
 fopen(s); %opens the serial port
-R = 0;
-P = 0;
-% Square parallel to ground
-x = [-0.5 -0.5 0.5 0.5];
-y = [-0.5 0.5 0.5 -0.5];
-z = [0 0 0 0];
-c = [-0.5 -0.5 0.5 0.5];
-% Plot plane
-p = patch (x, y, z, c);
-% Three axis limits
-xlim([-1 1])
-ylim([-1 1])
-zlim([-1 1])
 grid on
 % Axis labels
-xlabel('X')
-ylabel('Y')
-zlabel('Z')
-% Pause while configuring 3D plot (continue pressing any key*) *Enter only
-% tried
+xlabel('Time [s]')
+ylabel('Gravity [m/s^2]')
+hold on
 pause
 % Loop (cancel with 'Ctrl+C')
+tic
+i = 1;
+time = [];
 while 1
-    data = fgetl(s);
+    data = fscanf(s);
+    flushinput(s);
     C = strsplit(data,',');
     % Pitch in g
     P = str2double(cell2mat(C(1)));
     % Roll in g
     R = str2double(cell2mat(C(2)));
-    % Pitch angular components in degrees
-    aP = cosd(asind(P));
-    bP = sind(asind(P));
-    % Roll angular components in degrees
-    aR = cosd(asind(R));
-    bR = sind(asind(R));
-    x = [-0.5*aP -0.5*aP 0.5*aP 0.5*aP];        % PITCH ALONE WORKING
-%     z = [0.5*bP 0.5*bP -0.5*bP -0.5*bP];      % PITCH ALONE WORKING
-    z = [0.5*bP-0.5*bR 0.5*bP+0.5*bR -0.5*bP+0.5*bR -0.5*bP-0.5*bR];    % PITCH & ROLL WORKING
-    y = [-0.5*aR 0.5*aR 0.5*aR -0.5*aR];        % ROLL ALONE WORKING
-%     z = [-0.5*b 0.5*b 0.5*b -0.5*b];          % ROLL ALONE WORKING
-    % Clear plot
-    delete(p)
-    % Refresh plot
-    p = patch(x, y, z, c)
+    t = toc;
+    plot(t, R, '-*g')
+    plot(t, P, '-*r')
+    Pitch(i,:) = [t,P];
+    Roll(i,:) = [t,R];
+    drawnow;
     % Pause for matlab rest
-    pause(0.01);
+    i = i + 1;
+    pause(0.005);
 end
+% csvwrite(sFile,data);
